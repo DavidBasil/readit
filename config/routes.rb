ebfa75d5006b98d71119def97e1216c13b96fab1
David Basil <info@basildavid.com>
2018-03-01 14:24:07 +0400

    Add user and user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+	resource :session
+
 	resources :stories do
 		resources :votes
 	end
